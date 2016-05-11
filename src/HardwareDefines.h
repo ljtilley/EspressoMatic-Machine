0bae6b44d258b6d7de96bb000a6217568793add1
Luke Tilley <luketilley@gmail.com>
2016-05-10 20:33:12 -0700

    add specific hardware defines

---
+++ src/HardwareDefines.h
@@ -0,0 +1,9 @@
+// General I/O
+// production LED PIN
+// #define STATE_LED 8
+// Development LED Pin (arduino nano on-board)
+#define STATE_LED 13
+#define STEAM_SW 7
+#define BREW_SW 6
+#define BOIL_CTL 4
+#define PUMP_CTL 5
